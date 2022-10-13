be98ef11393bbd12d8ac9ad807429f66746b7794
nelly476 <nellybulatova12@gmail.com>
2022-10-13 22:55:37 +0300

    add js

---
+++ index.js
@@ -0,0 +1,98 @@
+import { catsData } from "/data.js";
+const emotionRadios = document.querySelector("#emotion-radios");
+const getImageBtn = document.querySelector("#get-image-btn");
+const gifsOnlyOption = document.querySelector("#gifs-only-option");
+const memeModalInner = document.getElementById("meme-modal-inner");
+const memeModal = document.getElementById("meme-modal");
+const modalCloseBtn = document.getElementById("meme-modal-close-btn");
+
+emotionRadios.addEventListener("change", highlightCheckedOption);
+
+modalCloseBtn.addEventListener("click", closeModal);
+
+getImageBtn.addEventListener("click", renderCat);
+
+function highlightCheckedOption(e) {
+  const radios = document.getElementsByClassName("radio");
+  for (let radio of radios) {
+    radio.classList.remove("highlight");
+  }
+
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
+}
+
+function closeModal() {
+  memeModal.style.display = "none";
+}
+
+function renderCat() {
+  const catObject = getSingleCatObject();
+  memeModalInner.innerHTML = `
+  <img 
+  class="cat-img" 
+  src="./images/${catObject.image}"
+  alt="CAT ALT TEXT"
+  >
+  `;
+  memeModal.style.display = "flex";
+}
+
+function getSingleCatObject() {
+  const catsArray = getMatchingCatsArray();
+  const randomIndex = Math.floor(Math.random() * catsArray.length);
+  if (catsArray.length === 1) {
+    return catsArray[0];
+  } else {
+    return catsArray[randomIndex];
+  }
+}
+
+function getMatchingCatsArray() {
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector(
+      'input[type="radio"]:checked'
+    ).value;
+    const isGif = gifsOnlyOption.checked;
+
+    const matchingCatsArray = catsData.filter(function (cat) {
+      if (isGif) {
+        return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
+      } else {
+        return cat.emotionTags.includes(selectedEmotion);
+      }
+    });
+    return matchingCatsArray;
+  }
+}
+
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
+    }
+  }
+  return emotionsArray;
+}
+
+function renderEmotionsRadios(cats) {
+  const emotions = getEmotionsArray(cats);
+  let renderedEmotions = ``;
+
+  for (let emotion of emotions) {
+    renderedEmotions += `
+    <div class="radio">
+    <label for=${emotion}>${emotion}</label>
+    <input type="radio"
+     id=${emotion}
+     value=${emotion}
+     name="emotions">
+</div>`;
+  }
+  emotionRadios.innerHTML = renderedEmotions;
+}
+
+renderEmotionsRadios(catsData);
